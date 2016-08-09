54c25975da5c2b3db1851a853e63a4a1796fce74
uelzki <uelzki@gmail.com>
2016-08-09 22:01:54 +0800

    Test1
    Test1 Description

---
+++ WinGame/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wb
+            // 
+            this.wb.Location = new System.Drawing.Point(31, 38);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.Size = new System.Drawing.Size(666, 422);
+            this.wb.TabIndex = 0;
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Location = new System.Drawing.Point(537, 479);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(142, 37);
+            this.PlayBtn.TabIndex = 1;
+            this.PlayBtn.Text = "PLAY";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 528);
+            this.Controls.Add(this.PlayBtn);
+            this.Controls.Add(this.wb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.Button PlayBtn;
+    }
+}
+
---
+++ WinGame/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+
+namespace WinGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            //wb.Url="http://www.miniclip.com/games/raiders-took-my-dog/en/#t-l-gs1-H";
+            this.wb.Navigate("http://www.miniclip.com/games/raiders-took-my-dog/en/#t-l-gs1-H");
+        }
+    }
+}
